b8f5772d517b2ddb578adc79066226c6d910a689
Prime Tecnologia <94810700+BrunoEduardo45@users.noreply.github.com>
2026-02-25 22:20:06 -0300

    implement JWT authentication, claims and api key authorization
    - Added User entity with password hashing
    - Implemented login endpoint using email and password
    - Generated JWT tokens with claims
    - Protected endpoints using Authorize attribute
    - Implemented API Key authentication as alternative access strategy
    - Configured authentication and authorization middleware

--- Data/Mappings/UserMap.cs
+++ Data/Mappings/UserMap.cs
@@ -9,24 +9,40 @@ namespace Blog.Data.Mappings
     {
         public void Configure(EntityTypeBuilder<User> builder)
         {
+            // Tabela
             builder.ToTable("User");
 
+            // Chave Primária
             builder.HasKey(x => x.Id);
 
+            // Identity
             builder.Property(x => x.Id)
                 .ValueGeneratedOnAdd()
                 .UseIdentityColumn();
 
+            // Propriedades
             builder.Property(x => x.Name)
                 .IsRequired()
                 .HasColumnName("Name")
                 .HasColumnType("NVARCHAR")
                 .HasMaxLength(80);
 
-            builder.Property(x => x.Bio);
-            builder.Property(x => x.Email);
-            builder.Property(x => x.Image);
-            builder.Property(x => x.PasswordHash);
+            builder.Property(x => x.Bio)
+                .IsRequired(false);
+
+            builder.Property(x => x.Email)
+                .IsRequired()
+                .HasColumnName("Email")
+                .HasColumnType("VARCHAR")
+                .HasMaxLength(160);
+
+            builder.Property(x => x.Image)
+                .IsRequired(false);
+
+            builder.Property(x => x.PasswordHash).IsRequired()
+                .HasColumnName("PasswordHash")
+                .HasColumnType("VARCHAR")
+                .HasMaxLength(255);
 
             builder.Property(x => x.Slug)
                 .IsRequired()
@@ -34,10 +50,12 @@ namespace Blog.Data.Mappings
                 .HasColumnType("VARCHAR")
                 .HasMaxLength(80);
 
+            // Índices
             builder
                 .HasIndex(x => x.Slug, "IX_User_Slug")
                 .IsUnique();
 
+            // Relacionamentos
             builder
                 .HasMany(x => x.Roles)
                 .WithMany(x => x.Users)
--- obj/Debug/net10.0/Blog.AssemblyInfo.cs
+++ obj/Debug/net10.0/Blog.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Blog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9e21cbcf699604c8fdc6beb5b3fb4d1b52f5c58")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8141135b91050e9c545c5f2ea879d940f25415c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Blog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Blog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
